4379f7bbbe4aa878909b79db8ae0dc0bfffc60a7
“SSONGNOSS66” <ssongnoss66@gmail.com>
2023-05-21 02:00:48 +0900

    HOST FIX

--- FILMA/settings.py
+++ FILMA/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://port-0-filmarchive-lhe2blhw8bzlc.sel4.cloudtype.app/']
 
 
 # Application definition
